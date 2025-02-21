3aadf344cba2248a7ad3ae5b1cda24b0f6263cf4
Nfcreaction <83551037+Nfcreaction@users.noreply.github.com>
2025-02-21 00:44:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,47 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
-dotenv.config();
-
+const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
-
+dotenv.config();
 var led = "OFF";
 var humedad;
 var temperatura;
 app.get('/', (req, res)=> {
   res.render('index');
 })
-
 app.get('/state/:temperatura/:humedad', (req, res)=>{
-  console.log(req.params.temperatura)
-  console.log(req.params.humedad)
   humedad = req.params.humedad
   temperatura = req.params.temperatura
-  console.log(led)
   res.send(led)  
+  console.log(`
+  *Consulta realizada: Estado LED y set de temp&humedad.*
+  -LED: ${LED}
+  -TEMP: ${temperatura}
+  -HUMEDAD: ${humedad}
+  `)
 })
-
 app.post('/eventosLed', (req,res)=>{
   led = req.body.state
-  console.log(req.body.state)
-  console.log(led)
+  console.log(`
+  *Consulta realizada: Definicio del estado LED*
+  -LED: ${led}
+  `)
 })
 app.get('/tiempo', (req,res)=>{
   res.json({
     "temperatura": temperatura,
     "humedad": humedad
   })
+  console.log(`
+  *Consulta realizada: Solicitud del estado de la temp&humd.*
+  -TEMP: ${temperatura}
+  -HUMD: ${humedad}
+  `)
 })
-
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
